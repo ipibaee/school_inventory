db01dab3451f4ea986ff7f8ddea93bc3614751f4
ipibaee <muamarzulkifli68@gmail.com>
2026-03-04 10:00:15 +0700

    chore: add vercel speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/sonner"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
                 {children}
                 <Toaster />
                 <Analytics />
+                <SpeedInsights />
             </body>
         </html>
     );
